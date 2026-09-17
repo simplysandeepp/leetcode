f808cc3ab7819c33235aba4692c070e4d3ebe1d8
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-09-17 21:44:52 +0530

    Time: 327 ms (49.02%), Space: 0B (100%) - LeetHub

---
+++ 3475-dna-pattern-recognition/3475-dna-pattern-recognition.sql
@@ -0,0 +1,7 @@
+select  sample_id, dna_sequence, species, 
+   (case when dna_sequence like "ATG%" then 1  else 0 end) as has_start,
+   (case when regexp_like (dna_sequence,"TAA$|TAG$|TGA$") then 1 else 0 end) as has_stop,
+   (case when dna_sequence like "%ATAT%" then 1 else 0 end) as has_atat,
+   (case when dna_sequence like "%GGG%" then 1 else 0 end) as has_ggg
+from Samples
+group by sample_id;
